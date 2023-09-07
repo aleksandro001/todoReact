b887eecb6f3427aa4fa4bdd0d8edcdf2b48afc8d
aleksandro001 <001aleksandro@gmail.com>
2023-09-07 21:39:38 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React   from "react";
+import ReactDOM from  'react-dom/client';
+import './toDo.css';
+import { formatDistanceToNow } from 'date-fns';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Task = (props) => {
+    return (<li>
+        <div className="view">
+            <input className="toggle" type="checkbox"/>
+            <label>
+                <span className="description">{props.name}</span>
+                <span className="created">created 5 minutes ago</span>
+            </label>
+            <button className="icon icon-edit"></button>
+            <button className="icon icon-destroy"></button>
+        </div>
+    </li>)
+}
+const TaskList = () => {
+    const completed = 'Completed task'
+    const editing = 'Editing task'
+    const active = 'Active task'
+    return (<ul className="todo-list">
+    <Task name={completed}/>
+    <Task name={editing}/>
+    <Task name={active}/>
+</ul>)
+}
+const NewTaskForm = () => {
+    return <input className="new-todo" placeholder="What needs to be done?" autoFocus/>
+}
+const Footer = () => {
+    return (<footer className="footer">
+    <span className="todo-count">1 items left</span>
+    <TasksFilter/>
+    <button className="clear-completed">Clear completed</button>
+</footer>)
+}
+const TasksFilter = () => {
+    return (<ul className="filters">
+    <li>
+        <button className="selected">All</button>
+    </li>
+    <li>
+        <button>Active</button>
+    </li>
+    <li>
+        <button>Completed</button>
+    </li>
+</ul>)
+}
+
+const el = (
+<section className="todoapp">
+    <header className="header">
+        <h1>todos</h1>
+        <NewTaskForm />
+    </header>
+    <section className="main">
+        <TaskList />
+        <Footer/>
+    </section>
+</section>
+
+)
+
+const distanceTime = formatDistanceToNow(new Date(), { addSuffix: true })
+console.log(distanceTime)
+
+root.render(el)
