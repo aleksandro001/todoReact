d0c74ef3e06806a2447327850aca1d66492cb3ae
aleksandro001 <001aleksandro@gmail.com>
2023-09-27 14:40:52 +0300

    lint-staged for 1.5.5

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,50 +1,43 @@
-import React, {Component} from "react";
-import classNames from "classnames";
+import React, { Component } from 'react';
+import classNames from 'classnames';
 import { formatDistanceToNow } from 'date-fns';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 export default class Task extends Component {
-    static defaultProps = {
-        onDeleted: () => {},
-        task: {},
-        completed: false,
-        onToggleDone: () => {},
-        createDate: () => {},
-    }
-    render() {
-        const {onDeleted, task, completed,  onToggleDone, createDate} = this.props
-        const itemClasses = classNames({
-            completed: completed,
-        })
-        const getFormattedDate = formatDistanceToNow(createDate,{addSuffix: true})
+  static defaultProps = {
+    onDeleted: () => {},
+    task: {},
+    completed: false,
+    onToggleDone: () => {},
+    createDate: () => {},
+  };
+  render() {
+    const { onDeleted, task, completed, onToggleDone, createDate } = this.props;
+    const itemClasses = classNames({
+      completed: completed,
+    });
+    const getFormattedDate = formatDistanceToNow(createDate, { addSuffix: true });
 
-        return (
-            <li className={itemClasses}>
-            <div className="view">
-                <input className="toggle" type="checkbox" checked={completed} onChange={onToggleDone}/>
-                <label>
-                    <span
-                        className="description"
-                        onClick={onToggleDone}
-                    >{task}</span>
-                    <span className="created">{ getFormattedDate}</span>
-                </label>
-                <button className="icon icon-edit"></button>
-                <button className="icon icon-destroy"
-                        onClick={onDeleted}
-                ></button>
-            </div>
-        </li>)
-    }
+    return (
+      <li className={itemClasses}>
+        <div className="view">
+          <input className="toggle" type="checkbox" checked={completed} onChange={onToggleDone} />
+          <label>
+            <span className="description" onClick={onToggleDone}>
+              {task}
+            </span>
+            <span className="created">{getFormattedDate}</span>
+          </label>
+          <button className="icon icon-edit"></button>
+          <button className="icon icon-destroy" onClick={onDeleted}></button>
+        </div>
+      </li>
+    );
+  }
 }
 Task.propTypes = {
-    onDeleted: PropTypes.func,
-    task: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number,
-    ]),
-    completed: PropTypes.bool,
-    onToggleDone: PropTypes.func,
-    createDate: PropTypes.instanceOf(Date),
-
-
-}
+  onDeleted: PropTypes.func,
+  task: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  completed: PropTypes.bool,
+  onToggleDone: PropTypes.func,
+  createDate: PropTypes.instanceOf(Date),
+};
--- src/components/TaskFilter.js
+++ src/components/TaskFilter.js
@@ -1,38 +1,36 @@
-import React, {Component}  from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class TasksFilter extends Component {
-    static defaultProps = {
-        onFilterChange: () => {},
-        filterValue: {},
-    }
-    render(){
-const {onFilterChange, filterValue} = this.props
+  static defaultProps = {
+    onFilterChange: () => {},
+    filterValue: {},
+  };
+  render() {
+    const { onFilterChange, filterValue } = this.props;
 
-        return (<ul className="filters">
-            <li>
-                <button
-                    className={ filterValue === 'ALL' ? 'selected': ''}
-                    onClick={() => onFilterChange('ALL')}
-                >All</button>
-            </li>
-            <li>
-                <button
-                    className={ filterValue === 'ACTIVE' ? 'selected': ''}
-                    onClick={() => onFilterChange('ACTIVE')}
-                >Active</button>
-            </li>
-            <li>
-                <button
-                    className={ filterValue === 'COMPLETED' ? 'selected': ''}
-                    onClick={() => onFilterChange('COMPLETED')}
-                >Completed</button>
-            </li>
-        </ul>)
-    }
+    return (
+      <ul className="filters">
+        <li>
+          <button className={filterValue === 'ALL' ? 'selected' : ''} onClick={() => onFilterChange('ALL')}>
+            All
+          </button>
+        </li>
+        <li>
+          <button className={filterValue === 'ACTIVE' ? 'selected' : ''} onClick={() => onFilterChange('ACTIVE')}>
+            Active
+          </button>
+        </li>
+        <li>
+          <button className={filterValue === 'COMPLETED' ? 'selected' : ''} onClick={() => onFilterChange('COMPLETED')}>
+            Completed
+          </button>
+        </li>
+      </ul>
+    );
+  }
 }
 TasksFilter.protTypes = {
-    onFilterChange: PropTypes.func,
-    filterValue: PropTypes.string,
-}
-
+  onFilterChange: PropTypes.func,
+  filterValue: PropTypes.string,
+};
